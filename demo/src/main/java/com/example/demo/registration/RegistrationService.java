645926be4e934a67785fef3de89bd02c36e5a51e
Soufian Kkhennousse <soufian.khennousse@stud.uni-due.de>
2024-04-29 20:28:45 +0200

    password encryption

--- demo/src/main/java/com/example/demo/registration/RegistrationService.java
+++ demo/src/main/java/com/example/demo/registration/RegistrationService.java
@@ -1,14 +1,25 @@
 package com.example.demo.registration;
 
 
+import com.example.demo.user.UserAccount;
+import com.example.demo.user.UserAccountService;
+import com.example.demo.user.UserRole;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
 @Service
 public class RegistrationService {
 
+    private final EmailValidator emailValidator;
+    private final UserAccountService userAccountService;
 
 
     public String register(RegistrationRequest request) {
-        return "its working";
+        boolean isValidemail = emailValidator.test(request.getEmail());
+        if(!isValidemail) {
+            throw new IllegalStateException("Invalid email");
+        }
+        return userAccountService.signUpUser(new UserAccount(request.getFirstName(), request.getLastName(), request.getEmail(), request.getPassword(), UserRole.USER));
     }
 }
