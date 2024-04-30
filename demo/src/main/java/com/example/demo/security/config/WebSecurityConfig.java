21395a0d802bc8579bdd6993ca0998525ed534ba
Soufian Kkhennousse <soufian.khennousse@stud.uni-due.de>
2024-04-30 20:43:03 +0200

    unused imports removed

--- demo/src/main/java/com/example/demo/security/config/WebSecurityConfig.java
+++ demo/src/main/java/com/example/demo/security/config/WebSecurityConfig.java
@@ -1,25 +1,16 @@
 package com.example.demo.security.config;
 
 import com.example.demo.user.UserAccountService;
-import jakarta.annotation.PostConstruct;
-import jakarta.servlet.Filter;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @AllArgsConstructor
@@ -46,26 +37,5 @@ public class WebSecurityConfig {
     }
 
 
-
-
-    /*public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authz) -> authz.anyRequest().authenticated())
-                .httpBasic(withDefaults());
-        return http.build();
-    }
-
-
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(daoAuthenticationProvider());
-    }
-
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
-            DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-            provider.setPasswordEncoder(bCryptPasswordEncoder);
-            provider.setUserDetailsService(userAccountService);
-            return provider;
-    }*/
 
 }
