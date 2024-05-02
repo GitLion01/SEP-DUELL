db1703102f28c46897fd0874ee4454c94f565a55
Saman Schero <saman.schero@uni-due.de>
2024-05-02 21:52:41 +0200

    kleine Änderungen

--- login-register/src/App.jsx
+++ login-register/src/App.jsx
@@ -3,20 +3,20 @@ import LoginPage from './components/Login/LoginPage';
 import RegisterPage from './components/RegisterPage';
 import SuccesfulLog from './components/Login/SuccessfulLog';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import TwoFaktorAuthenfication from './components/2FA/TwoFaktorAuthenfication';
-
+import TwoFaktorAuthenfication from './components/2FA/TwoFactorAuthentication';
+import Profile from './components/Profileinsicht/Profile';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/registration" element={<RegisterPage />} />
         <Route path="/startseite" element={<SuccesfulLog/>} /> {/* Die Namen der Seiten müssen groß geschrieben werden, damit das klappt*/}
         <Route path="/2fa" element={<TwoFaktorAuthenfication/>} />
+        <Route path="/profile" element={<Profile/>} />
       </Routes>
     </Router>
   );
 }
-
 export default App;
--- login-register/src/components/2FA/TwoFactorAuthentication.jsx
+++ login-register/src/components/2FA/TwoFactorAuthentication.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './TwoFaktorAuthenfication.css';
+import './TwoFactorAuthentication.css';
 
 class TwoFaktorAuthenfication extends Component {
     state = {
