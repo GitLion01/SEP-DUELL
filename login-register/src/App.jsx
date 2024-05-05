3faec18a5054d7a0e5cc7e765ffea86593d15db0
Saman Schero <saman.schero@uni-due.de>
2024-05-05 15:31:37 +0200

    Merge Beyza+Saman+Enes

--- login-register/src/App.jsx
+++ login-register/src/App.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import LoginPage from './components/Login/LoginPage';
-import RegisterPage from './components/RegisterPage';
-import SuccesfulLog from './components/Login/SuccessfulLog';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TwoFaktorAuthenfication from './components/2FA/TwoFactorAuthentication';
 import Profile from './components/Profileinsicht/Profile';
+import Register from './components/Registrierung/Register.jsx' 
+import Startseite from './components/Startseite/Startsite.jsx';
+import DeckEditor from './components/Deckarbeiten/DeckEditor.jsx';
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/registration" element={<RegisterPage />} />
-        <Route path="/startseite" element={<SuccesfulLog/>} /> {/* Die Namen der Seiten müssen groß geschrieben werden, damit das klappt*/}
+        <Route path="/registration" element={<Register/>} /> 
+        <Route path="/startseite" element={<Startseite/>} /> {/* Die Namen der Seiten müssen groß geschrieben werden, damit das klappt*/}
         <Route path="/2fa" element={<TwoFaktorAuthenfication/>} />
         <Route path="/profile" element={<Profile/>} />
+        <Route path='/deckbearbeiten' element = {<DeckEditor/>} /> 
       </Routes>
     </Router>
   );
