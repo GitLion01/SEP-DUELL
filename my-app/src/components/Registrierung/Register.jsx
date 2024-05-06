d49879c95068d133c2c12cd91e43316e9677c830
Soufian Kkhennousse <soufian.khennousse@stud.uni-due.de>
2024-05-06 22:49:00 +0200

    Admin checkable and uncheckable

--- my-app/src/components/Registrierung/Register.jsx
+++ my-app/src/components/Registrierung/Register.jsx
@@ -1,106 +1,114 @@
-import React, { Component } from 'react';
-import './Register.css';
+import React, { Component } from 'react'; // Importiere React und Component aus der 'react'-Bibliothek
+import './Register.css'; // Importiere das Styling für diese Komponente
 
+
+
+// Definiere die Register-Komponente und render die Registrierung-Komponente
+class Register extends Component {
+  render() { // Definiere die render-Methode
+    return <Registrierung />; // Rendere die Registrierung-Komponente innerhalb von Register
+  }
+}
+
+
+
+// Deklariere die Registrierungskomponente
+//
 class Registrierung extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      image: null,
-      previewImage: null,
-      formData: { // Neues formData-Objekt im state
-        username: '',
-        firstName: '',
-        lastName: '',
-        dateOfBirth: '',
-        email: '',
-        password: '', // Passwortfeld im formData hinzugefügt
-        role: ""
+  constructor(props) { // Konstruktor für die Registrierungskomponente
+    //props sind die Eigenschaften, die der Komponente übergeben werden können, wenn sie verwendet wird.
+    super(props); // Aufruf des Konstruktors der Elternklasse
+    this.state = { // Setze den Anfangszustand der Komponente fest
+      // Der Zustand (state) ist ein Objekt, das die Daten enthält, die die Komponente verwalten soll
+      //state) ist ein zentrales Konzept in React und wird verwendet, um Daten zu speichern,
+      //die sich im Laufe der Zeit ändern können
+      // und die das Aussehen und Verhalten der Benutzeroberfläche beeinflussen
+      image: null, // Das ausgewählte Bild
+      previewImage: null, // Die Vorschau des ausgewählten Bildes
+      formData: { // Formulardaten
+        username: '', // Benutzername
+        firstName: '', // Vorname
+        lastName: '', // Nachname
+        dateOfBirth: '', // Geburtsdatum
+        email: '', // E-Mail
+        password: '', // Passwort
+        role: "" // Rolle (Administrator oder Benutzer)
       },
+      role: false,
     };
   }
 
-
-  handleSubmit = async (event) => {
-    event.preventDefault();
-    const formData = this.state.formData;
-    // Hier kannst du die Logik für die Datenverarbeitung einfügen
+  // Methode zum Verarbeiten des Formularabsendens
+  handleSubmit = async (event) => { // Methode für das Absenden des Formulars
+    event.preventDefault(); // Verhindere das Standardverhalten des Formulars
+    const formData = this.state.formData; // Greife auf die Formulardaten im Zustand zu
     try {
-
-      formData.role = this.state.role ? 'ADMIN' : 'USER';
-      const response = await fetch('http://localhost:8080/registration', {
-        method: 'POST',
+      formData.role = this.state.role ? 'ADMIN' : 'USER'; // Setze die Rolle basierend auf dem Checkbox-Zustand
+      const response = await fetch('http://localhost:8080/registration', { // Sende die Formulardaten an den Server
+        method: 'POST', // HTTP-Methode
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json' // Header für JSON-Daten
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify(formData) // Formulardaten in JSON umwandeln und senden
       });
 
-      if (!response.ok) {
+      if (!response.ok) { // Wenn die Antwort nicht erfolgreich ist, wirf einen Fehler
         throw new Error('Registrierung fehlgeschlagen');
       }
 
-      console.log('Registrierung erfolgreich');
+      console.log('Registrierung erfolgreich'); // Logge eine Erfolgsmeldung
       // Hier könntest du zur Login-Seite weiterleiten oder andere Aktionen nach erfolgreicher Registrierung durchführen
     } catch (error) {
-      console.error('Fehler bei der Registrierung:', error.message);
+      console.error('Fehler bei der Registrierung:', error.message); // Logge Fehlermeldungen
     }
   };
 
-  //HIER GHETS UM DIE VERÄNDERUNG DES BILDES UND DIE DIE BILD EINFÜGUNG
-  handleImageChange = (event) => {
-    const file = event.target.files[0];
+  // Methode zur Verarbeitung der Bildauswahl und Anzeige der Vorschau
+  handleImageChange = (event) => { // Methode für die Änderung des Bildes
+    const file = event.target.files[0]; // Das ausgewählte Bild
     if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
+      const reader = new FileReader(); // FileReader-Objekt zum Lesen von Dateien
+      reader.onloadend = () => { // Wenn das Lesen abgeschlossen ist
         this.setState({
-          image: file,
-          previewImage: reader.result
+          image: file, // Setze das ausgewählte Bild
+          previewImage: reader.result // Setze die Vorschau des ausgewählten Bildes
         });
       };
-      reader.readAsDataURL(file);
+      reader.readAsDataURL(file); // Lese das ausgewählte Bild als Data-URL
     }
   };
 
-  //HIER GEHT ES UM DIE VERÄNDERUNG DER CHECKBOX!
-  //HIER GEHT ES UM DIE VERÄNDERUNG DER CHECKBOX!
-  handleRoleChange = () => {
+  // Methode zur Verarbeitung der Änderung der Checkbox
+  handleRoleChange = () => { // Methode für die Änderung der Rolle
     this.setState(prevState => ({
-
-        ...prevState.formData,
-        role: prevState.formData.role === 'ADMIN' ? 'USER' : 'ADMIN'
-
+      role:!prevState.role, // Toggle the role state
+      formData: {
+        ...prevState.formData, // Aktualisiere den vorherigen Zustand der Formulardaten
+        role: prevState.role === 'ADMIN' ? 'USER' : 'ADMIN' // Ändere die Rolle
+      },
     }));
   };
 
-
-
-
-
-  handleChange = (event) => {
-    const { name, value } = event.target;
+  // Methode zur Verarbeitung der Änderung von Formularelementen
+  handleChange = (event) => { // Methode für die Änderung von Formularelementen
+    const { name, value } = event.target; // Extrahiere Name und Wert des Elements
     this.setState(prevState => ({
       formData: {
-        ...prevState.formData,
-        [name]: value
+        ...prevState.formData, // Aktualisiere den vorherigen Zustand der Formulardaten
+        [name]: value // Setze den neuen Wert für das entsprechende Formularelement
       }
     }));
   };
 
-
-
-
-  render() {
+  render() { // Methode zum Rendern der JSX
     return (
-
-        /*HIER WIRD DER HAUPT CONTAINER DER REGISTRIERUNG ERSTELLT*/
-
+        /* Hauptcontainer für die Registrierung */
         <div className="container">
-
-          {/* ÜBERSCHRIFT  CONTAINER*/}
+          {/* Überschrift für die Registrierung */}
           <h2>REGISTRIERUNG</h2>
           <form onSubmit={this.handleSubmit}>
 
-            { /*PROFILBILD LABEL & EINGABEFELD & AUSSEHEN */}
+            {/* Profilbildauswahl */}
             <div className="form-group">
               <label htmlFor="image">Profilbild:</label>
               <input type="file" id="image" name="image" accept="image/*" onChange={this.handleImageChange} />
@@ -108,49 +116,55 @@ class Registrierung extends React.Component {
                   <img src={this.state.previewImage} alt="Preview" style={{ width: '100px', marginTop: '10px', height: '100px', borderRadius: '100%' }} />
               )}
             </div>
-            {/*USERNAME LABEL & EINGABEFELD*/}
+
+            {/* Benutzername */}
             <div className="form-group">
               <label htmlFor="username">Username:</label>
               <input type="text" id="username" name="username" required  onChange={this.handleChange}/>
             </div>
-            {/*VORNAME LABEL & EINGABEFELD*/}
+
+            {/* Vorname */}
             <div className="form-group">
               <label htmlFor="firstName">Vorname:</label>
               <input type="text" id="firstName" name="firstName" required onChange={this.handleChange}/>
             </div>
 
-            {/*NACHNAME LABEL & EINGABEFELD*/}
+            {/* Nachname */}
             <div className="form-group">
               <label htmlFor="lastName">Nachname:</label>
               <input type="text" id="lastName" name="lastName" required onChange={this.handleChange}/>
             </div>
 
-            {/*GEBURTSDATUM LABEL & EINGABEFELD*/}
+            {/* Geburtsdatum */}
             <div className="form-group">
               <label htmlFor="dateOfBirth">Geburtsdatum:</label>
               <input type="date" id="dateOfBirth" name="dateOfBirth" required onChange={this.handleChange}/>
             </div>
 
-            {/*EMAIL LABEL & EINGABEFELD*/}
+            {/* E-Mail */}
             <div className="form-group">
               <label htmlFor="email">E-Mail:</label>
               <input type="email" id="email" name="email" required onChange={this.handleChange}/>
             </div>
 
-            {/*PASSWORT LABEL & EINGABEFELD*/}
+            {/* Passwort */}
             <div className="form-group">
               <label htmlFor="password">Passwort:</label>
               <input type="password" id="password" name="password" onChange={this.handleChange}/>
             </div>
 
-            {/*ADMIN LABEL & CHECKBOX*/}
-            <div className="form-group">
+            {/* Admin-Checkbox */}
+            <div  className="form-group">
               <label htmlFor="role">Admin?</label>
-              <input type="checkbox" id="role" checked={this.state.role} onChange={this.handleRoleChange} />
+              <input
+                  type="checkbox"
+                  id="role"
+                  checked={this.state.role}
+                  onChange={this.handleRoleChange}
+              />
             </div>
 
-
-            {/* BUTTON AM ENDE DER SEITE*/}
+            {/* Submit-Button */}
             <button type="submit">Registrieren</button>
           </form>
         </div>
@@ -158,11 +172,4 @@ class Registrierung extends React.Component {
   }
 }
 
-// Define the Register component and render the Registrierung component
-class Register extends Component {
-  render() {
-    return <Registrierung />;
-  }
-}
-
-export default Register;
+export default Register; // Exportiere die Register-Komponente
