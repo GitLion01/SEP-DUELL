53475225e5947d209545ec7733425168ba06dec8
Saman Schero <saman.schero@uni-due.de>
2024-05-07 00:18:24 +0200

    kleine Fehlerbehebung

--- templates/src/components/Adminsteuerfeld/Admin.jsx
+++ templates/src/components/Adminsteuerfeld/Admin.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axiosInstance from '../../api/axios';
 import './Admin.css'; // Stile für das Adminpanel
-import Card from '../Card'; 
+import Card from '../card'; 
 
 class AdminPanel extends Component {
   state = {
