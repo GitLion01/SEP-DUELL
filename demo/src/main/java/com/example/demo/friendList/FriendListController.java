0104998b774f54f9b9a1db2ccab195c0534bca6d
Danny Nasra <danny.nasra@stud.uni-due.de>
2024-05-08 18:03:49 +0200

    FrienshipRequest in Conrroller : change id from Pathvariable to RequestParam because it's not part of the path

--- demo/src/main/java/com/example/demo/friendList/FriendListController.java
+++ demo/src/main/java/com/example/demo/friendList/FriendListController.java
@@ -14,12 +14,12 @@ public class FriendListController {
 
     private final FriendListService friendListService;
     @GetMapping(path ="/{id}")
-    public Optional<List<UserAccount>> getFriends(@PathVariable int id) {
-        return friendListService.getFriendList(id);
+    public List<UserAccount> getFriends(@PathVariable int id) {
+        return friendListService.getFriendList(id).orElse(null);
     }
 
     @GetMapping
-    public String FriendshipRequest(@PathVariable int id, @RequestParam int friend_id) {
+    public String FriendshipRequest(@RequestParam int id, @RequestParam int friend_id) {
         return friendListService.FriendshipRequest(id,friend_id);
     }
 
