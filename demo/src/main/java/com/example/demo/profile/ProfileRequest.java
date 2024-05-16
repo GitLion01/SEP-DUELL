afe034f7b7daebd697ddfed4742694e02c197f54
Danny Nasra <danny.nasra@stud.uni-due.de>
2024-05-16 19:06:50 +0200

    Cards von ProfilRequest gelöscht

--- demo/src/main/java/com/example/demo/profile/ProfileRequest.java
+++ demo/src/main/java/com/example/demo/profile/ProfileRequest.java
@@ -1,11 +1,8 @@
 package com.example.demo.profile;
-import com.example.demo.cards.Card;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
-
-import java.util.List;
 
 @Getter
 @AllArgsConstructor
@@ -15,6 +12,4 @@ public class ProfileRequest {
     private String name;
     private int SEPCoins;
     private int leaderboardPoints;
-    private int cards;
-    private List<Card> cardList;
 }
