670825316f8e12056db81cb01c15e62ae9fda95c
“Beyza <Beyza.Alhanolgu@stud.uni-due.de>
2024-05-17 13:25:50 +0200

    - Freundeliste überarbeitet => Nutzerliste und Freundeliste werden richtig angezeigt

--- templates/src/components/Freunde/Freundeliste.jsx
+++ templates/src/components/Freunde/Freundeliste.jsx
@@ -1,49 +1,63 @@
 import React, { useState, useEffect } from 'react';
 import Modal from 'react-modal';
 import './Freundeliste.css';
-import '../ProfileForFriends/ProfileFriends.css'
+import '../ProfileForFriends/ProfileFriends.css';
 Modal.setAppElement('#root'); // Set the root element for accessibility
 
 function App() {
   const [users, setUsers] = useState([]);
   const [friends, setFriends] = useState([]);
   const [selectedFriend, setSelectedFriend] = useState(null); // State for selected friend
-  const [selectedFriendFriends, setSelectedFriendFriends] = useState([]); 
+  const [selectedFriendFriends, setSelectedFriendFriends] = useState([]);
   const [modalIsOpen, setModalIsOpen] = useState(false); // State for modal open/close
 
+  const userId = localStorage.getItem('id'); // Get user ID from local storage
+
+  console.log("Current user ID:", userId);
+
   useEffect(() => {
-    fetchUsers();
-    fetchFriends();
+    const fetchData = async () => {
+      try {
+        const [friendsResponse, usersResponse] = await Promise.all([
+          fetchFriends(),
+          fetchUsers()
+        ]);
+
+        setFriends(friendsResponse);
+
+        const filteredUsers =
+            usersResponse.filter(user =>
+                user.id.toString() !== userId.toString() &&
+                !friendsResponse.some(friend => friend.id.toString() === user.id.toString())
+            );
+
+        setUsers(filteredUsers);
+      } catch (error) {
+        console.error('Fetch failed:', error.message);
+      }
+    };
+
+    fetchData();
   }, []);
 
-  const userId = localStorage.getItem('id');
-
   const fetchUsers = async () => {
-    try {
-      const url = `http://localhost:8080/registration/users`;
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const data = await response.json();
-      setUsers(data);
-    } catch (error) {
-      console.error('Fetch failed:', error.message);
+    const url = `http://localhost:8080/registration/users`;
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
     }
+    const data = await response.json();
+    return data;
   };
 
   const fetchFriends = async () => {
-    try {
-      const url = `http://localhost:8080/friendlist/${userId}`;
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const data = await response.json();
-      setFriends(data);
-    } catch (error) {
-      console.error('Fetch failed:', error.message);
+    const url = `http://localhost:8080/friendlist/${userId}`;
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
     }
+    const data = await response.json();
+    return data;
   };
 
   const sendFriendRequest = (friendId) => {
@@ -57,18 +71,16 @@ function App() {
     };
 
     fetch(url, requestData)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        console.log(`Freundschaftsanfrage an ${friendId} gesendet`);
-        alert('Freundschaftsanfrage wurde gesendet')
-
-        
-      })
-      .catch(error => {
-        console.error('There was a problem with the network request:', error);
-      });
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          console.log(`Freundschaftsanfrage an ${friendId} gesendet`);
+          alert('Freundschaftsanfrage wurde gesendet');
+        })
+        .catch(error => {
+          console.error('There was a problem with the network request:', error);
+        });
   };
 
   const fetchFriendsOfFriend = async (userId, friendId) => {
@@ -91,7 +103,6 @@ function App() {
     }
   };
 
-
   const removeFriend = (friendId) => {
     const url = 'http://localhost:8080/friendlist/remove';
     const requestData = {
@@ -103,24 +114,22 @@ function App() {
     };
 
     fetch(url, requestData)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log('Backend response:', data);
-        const index = friends.findIndex(f => f.id === friendId);
-        const updatedFriends = [...friends.slice(0, index), ...friends.slice(index + 1)];
-        setFriends(updatedFriends);
-        console.log(`Freund mit ID ${friendId} wurde aus der Freundesliste entfernt`);
-        alert('Freund wurde aus der Freundesliste entfernt')
-
-      })
-      .catch(error => {
-        console.error('There was a problem with the network request:', error);
-      });
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(data => {
+          console.log('Backend response:', data);
+          const updatedFriends = friends.filter(f => f.id !== friendId);
+          setFriends(updatedFriends);
+          console.log(`Freund mit ID ${friendId} wurde aus der Freundesliste entfernt`);
+          alert('Freund wurde aus der Freundesliste entfernt');
+        })
+        .catch(error => {
+          console.error('There was a problem with the network request:', error);
+        });
   };
 
   const openModal = (friend) => {
@@ -136,68 +145,68 @@ function App() {
   };
 
   return (
-    <div className="BFF">
-      <div className="UserList-hülle">
-        <h2>Nutzerliste</h2>
-        <div className="UserList">
-          <ul>
-            {users.map(user => (
-              <li key={user.id}>
-                {user.username}
-                <button onClick={() => sendFriendRequest(user.id)}>Add</button>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-      <div className="FriendList-hülle">
-        <h2>Meine Freunde</h2>
-        <div className="FriendList">
-          <ul>
-            {friends.map(friend => (
-              <li key={friend.id}>
-                <span onClick={() => openModal(friend)} style={{ cursor: 'pointer', color: 'blue' }}>{friend.username}</span>
-                <button onClick={() => removeFriend(friend.id)}>Freund entfernen</button>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        contentLabel="Friend Profile"
-        className="Modal"
-        overlayClassName="Overlay"
-      >
-        {selectedFriend && (
-          <div className={'profile-container'}>
-            <div className={'profile-header'}>
-              <h2>Profil von {selectedFriend.username}</h2>
-            </div>
-            <div className={'profile-info'}>
-              <img className={'profile-picture'} src={`data:image/jpeg;base64,${selectedFriend.profilePicture}`} alt="Profilbild" />
-              <p><strong>Benutzername:</strong> {selectedFriend.username}</p>
-              <p><strong>Vorname:</strong> {selectedFriend.firstName}</p>
-              <p><strong>Nachname:</strong> {selectedFriend.lastName}</p>
-              <p><strong>Leaderboard-Punkte:</strong> {selectedFriend.leaderboardPoints}</p>
-            </div>
-            <div className="friends-of-friend">
-              <h4>Freunde von {selectedFriend.username}</h4>
-              <div className="friends-list">
-                <ul>
-                  {selectedFriendFriends.map(friend => (
-                    <li key={friend.id}>{friend.username}</li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-            <button onClick={closeModal}>Schließen</button>
+      <div className="BFF">
+        <div className="UserList-hülle">
+          <h2>Nutzerliste</h2>
+          <div className="UserList">
+            <ul>
+              {users.map(user => (
+                  <li key={user.id}>
+                    {user.username}
+                    <button onClick={() => sendFriendRequest(user.id)}>Add</button>
+                  </li>
+              ))}
+            </ul>
           </div>
-        )}
-      </Modal>
-    </div>
+        </div>
+        <div className="FriendList-hülle">
+          <h2>Meine Freunde</h2>
+          <div className="FriendList">
+            <ul>
+              {friends.map(friend => (
+                  <li key={friend.id}>
+                    <span onClick={() => openModal(friend)} style={{ cursor: 'pointer', color: 'blue' }}>{friend.username}</span>
+                    <button onClick={() => removeFriend(friend.id)}>Freund entfernen</button>
+                  </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            contentLabel="Friend Profile"
+            className="Modal"
+            overlayClassName="Overlay"
+        >
+          {selectedFriend && (
+              <div className={'profile-container'}>
+                <div className={'profile-header'}>
+                  <h2>Profil von {selectedFriend.username}</h2>
+                </div>
+                <div className={'profile-info'}>
+                  <img className={'profile-picture'} src={`data:image/jpeg;base64,${selectedFriend.profilePicture}`} alt="Profilbild" />
+                  <p><strong>Benutzername:</strong> {selectedFriend.username}</p>
+                  <p><strong>Vorname:</strong> {selectedFriend.firstName}</p>
+                  <p><strong>Nachname:</strong> {selectedFriend.lastName}</p>
+                  <p><strong>Leaderboard-Punkte:</strong> {selectedFriend.leaderboardPoints}</p>
+                </div>
+                <div className="friends-of-friend">
+                  <h4>Freunde von {selectedFriend.username}</h4>
+                  <div className="friends-list">
+                    <ul>
+                      {selectedFriendFriends.map(friend => (
+                          <li key={friend.id}>{friend.username}</li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+                <button onClick={closeModal}>Schließen</button>
+              </div>
+          )}
+        </Modal>
+      </div>
   );
 }
 
