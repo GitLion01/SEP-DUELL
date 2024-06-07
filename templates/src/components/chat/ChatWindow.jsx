c17b5c377906c09a74dc83853c410e5334afa0b9
Saman Schero <saman.schero@uni-due.de>
2024-06-07 12:04:43 +0200

    Fehlerbehebung

--- templates/src/components/chat/ChatWindow.jsx
+++ templates/src/components/chat/ChatWindow.jsx
@@ -13,28 +13,26 @@ function ChatWindow({ friend, type }) {
   const [editingMessage, setEditingMessage] = useState(null);
   const userId = localStorage.getItem('id');
 
-  if (!friend || !friend.id) {
-    return <div>Wählen Sie einen Freund oder eine Gruppe aus, um den Chat zu starten</div>;
-  }
-
-  const chatId = generateChatId(userId, friend.id);
+  const chatId = friend && friend.id ? generateChatId(userId, friend.id) : null;
 
   // Nachrichten vom Server laden
   useEffect(() => {
-    const fetchMessages = async () => {
-      try {
-        const response = await fetch(`http://localhost:8080/messages/${chatId}`);
-        const data = await response.json();
-        setMessages((prevMessages) => ({
-          ...prevMessages,
-          [chatId]: Array.isArray(data) ? data : [] // Ensure data is an array
-        }));
-      } catch (error) {
-        console.error("Fehler beim Laden der Nachrichten:", error);
-      }
-    };
+    if (chatId) {
+      const fetchMessages = async () => {
+        try {
+          const response = await fetch(`http://localhost:8080/messages/${chatId}`);
+          const data = await response.json();
+          setMessages((prevMessages) => ({
+            ...prevMessages,
+            [chatId]: Array.isArray(data) ? data : [], // Ensure data is an array
+          }));
+        } catch (error) {
+          console.error('Fehler beim Laden der Nachrichten:', error);
+        }
+      };
 
-    fetchMessages();
+      fetchMessages();
+    }
   }, [chatId]);
 
   // WebSocket-Verbindung herstellen und Nachrichten empfangen
@@ -71,7 +69,7 @@ function ChatWindow({ friend, type }) {
         text: newMessage,
         timestamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
         chatId: chatId,
-        read: false
+        read: false,
       };
       webSocketService.sendMessage(JSON.stringify(message));
 
@@ -100,7 +98,7 @@ function ChatWindow({ friend, type }) {
 
       setMessages((prevMessages) => {
         const updatedMessages = { ...prevMessages };
-        updatedMessages[chatId] = updatedMessages[chatId].map(msg =>
+        updatedMessages[chatId] = updatedMessages[chatId].map((msg) =>
           msg.timestamp === editingMessage.timestamp ? updatedMessage : msg
         );
         return updatedMessages;
@@ -116,7 +114,7 @@ function ChatWindow({ friend, type }) {
 
     setMessages((prevMessages) => {
       const updatedMessages = { ...prevMessages };
-      updatedMessages[chatId] = updatedMessages[chatId].filter(msg => msg.timestamp !== message.timestamp);
+      updatedMessages[chatId] = updatedMessages[chatId].filter((msg) => msg.timestamp !== message.timestamp);
       return updatedMessages;
     });
   };
@@ -139,7 +137,7 @@ function ChatWindow({ friend, type }) {
 
       setMessages((prevMessages) => {
         const updatedMessages = { ...prevMessages };
-        updatedMessages[chatId] = updatedMessages[chatId].map(msg =>
+        updatedMessages[chatId] = updatedMessages[chatId].map((msg) =>
           msg.timestamp === message.timestamp ? { ...msg, read: true } : msg
         );
         return updatedMessages;
@@ -149,27 +147,47 @@ function ChatWindow({ friend, type }) {
 
   const chatMessages = Array.isArray(messages[chatId]) ? messages[chatId] : [];
 
+  if (!friend || !friend.id) {
+    return <div>Wählen Sie einen Freund oder eine Gruppe aus, um den Chat zu starten</div>;
+  }
+
   return (
     <div className="chat-window">
       <h2>Chat mit {type === 'friend' ? friend.username : friend.name}</h2>
       <div className="chat-messages imessage">
         {chatMessages.map((message, index) => (
-          <p key={index}
-             className={message.sender === userId ? 'from-me' : 'from-them'}
-             onClick={() => handleReadMessage(message)}>
+          <p
+            key={index}
+            className={message.sender === userId ? 'from-me' : 'from-them'}
+            onClick={() => handleReadMessage(message)}
+          >
             {type === 'group' && message.sender !== userId && (
               <span className="message-sender">{message.senderName}</span>
             )}
             <span className="message-text">{message.text}</span>
             <div className="message-actions">
               <button onClick={() => handleEditMessage(message)}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16">
-                  <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-pencil-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z" />
                 </svg>
               </button>
               <button onClick={() => handleDeleteMessage(message)}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
-                  <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-trash-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
                 </svg>
               </button>
               <span className="message-timestamp">{message.timestamp}</span>
