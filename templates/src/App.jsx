026f2bdd67d4a15b0c43cbbe89175ad7908d0861
Enes Coskun <enes.coskun@stud.uni-due.de>
2024-06-08 22:10:05 +0200

    WebSocket Client in App.jsx übertragen (ungetestet)

--- templates/src/App.jsx
+++ templates/src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import LoginPage from './components/Login/LoginPage';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Client } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
+
+import LoginPage from './components/Login/LoginPage';
 import TwoFaktorAuthenfication from './components/2FA/TwoFactorAuthentication';
-import Register from './components/Registrierung/Register.jsx' 
+import Register from './components/Registrierung/Register.jsx';
 import Startseite from './components/Startseite/Startseite.jsx';
 import AdminPanel from './components/Adminsteuerfeld/Admin.jsx';
 import Profile from './components/Profilansicht/Profile.jsx';
 import Decks from './components/Deck-erstellen/Decks.jsx';
-import './index.css';
 import Freundeliste from "./components/Freunde/Freundeliste.jsx";
 import ProtectedRoute from './ProtectedRoute.jsx';
 import ShopPage from './components/ShopPage/ShopPage.jsx';
@@ -15,26 +17,60 @@ import ChatPage from './components/chat/ChatPage.jsx';
 import DeckSelection from './components/Duell/DeckSelection.jsx';
 import Duel from "./components/Duell/Duel.jsx";
 
+import './index.css';
 
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/registration" element={<Register/>} /> 
-        <Route path="/startseite" element={<ProtectedRoute element={Startseite} />} /> {/* Die Namen der Seiten müssen groß geschrieben werden, damit das klappt*/}
-        <Route path="/2fa" element={<TwoFaktorAuthenfication/>} />
-        <Route path="/profil" element={<ProtectedRoute element={Profile} />} />
-        <Route path="/admin" element={<ProtectedRoute element={AdminPanel} requiredRole="ADMIN" />} />
-        <Route path="/decks" element={<ProtectedRoute element={Decks} />} />
-        <Route path="/freundelist" element={<ProtectedRoute element={Freundeliste} />} />
-        <Route path="/shop" element={<ProtectedRoute element={ShopPage} />} />
-        <Route path="/chat" element={<ProtectedRoute element={ChatPage} />} /> {/* Neue Route für die Chat-Seite */}
+  const [client, setClient] = useState(null);
 
-         {/* Neue Routen für das Duell */}
-      </Routes>
-    </Router>
+  useEffect(() => {
+    const newClient = new Client({
+      brokerURL: 'ws://localhost:8080/ws',
+      webSocketFactory: () => new SockJS('http://localhost:8080/ws'),
+      onConnect: () => {
+        console.log('Connected to WebSocket server');
+      },
+      onDisconnect: () => {
+        console.log('Disconnected from WebSocket server');
+      }
+    });
+
+    newClient.activate();
+    setClient(newClient);
+
+    return () => {
+      newClient.deactivate();
+    };
+  }, []);
+
+  if (!client) {
+    return <div>Loading...</div>;
+  }
+
+
+
+  return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginPage/>}/>
+          <Route path="/registration" element={<Register/>}/>
+          <Route path="/startseite" element={<ProtectedRoute
+              element={Startseite}/>}/> {/* Die Namen der Seiten müssen groß geschrieben werden, damit das klappt*/}
+          <Route path="/2fa" element={<TwoFaktorAuthenfication/>}/>
+          <Route path="/profil" element={<ProtectedRoute element={Profile}/>}/>
+          <Route path="/admin" element={<ProtectedRoute element={AdminPanel} requiredRole="ADMIN"/>}/>
+          <Route path="/decks" element={<ProtectedRoute element={Decks}/>}/>
+          <Route path="/freundelist" element={<ProtectedRoute element={Freundeliste}/>}/>
+          <Route path="/shop" element={<ProtectedRoute element={ShopPage}/>}/>
+          <Route path="/chat" element={<ProtectedRoute element={ChatPage}/>}/> {/* Neue Route für die Chat-Seite */}
+
+          {/* Neue Routen für das Duell */}
+
+          <Route path="/deck-selection" element={<ProtectedRoute element={() => <DeckSelection client={client} />} />} />
+          <Route path="/duel" element={<ProtectedRoute element={() => <Duel client={client} />} />} />
+
+        </Routes>
+      </Router>
   );
 }
 export default App;
