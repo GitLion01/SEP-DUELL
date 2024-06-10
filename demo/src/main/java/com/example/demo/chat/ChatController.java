06d909262ba9067a1b7eaf2cdde43e9b69768485
Danny Nasra <danny.nasra@stud.uni-due.de>
2024-06-10 23:59:49 +0200

    a new Chat will be created only if there is no old chat between the users

--- demo/src/main/java/com/example/demo/chat/ChatController.java
+++ demo/src/main/java/com/example/demo/chat/ChatController.java
@@ -1,59 +1,30 @@
 package com.example.demo.chat;
-import com.example.demo.user.UserAccount;
-import com.example.demo.user.UserAccountRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
 
 @CrossOrigin
 @RestController
+@AllArgsConstructor
 public class ChatController {
 
-    @Autowired
-    private UserAccountRepository userAccount;
-    @Autowired
-    private ChatRepository chatRepository;
-    @Autowired
-    private ChatMessageRepository chatMessageRepository;
-    @Autowired
-    private SimpMessagingTemplate messagingTemplate;
+    private final ChatService chatService;
 
-    @Transactional
     @PostMapping("/create.chat")
-    public Chat createChat(@RequestParam Long userId1, @RequestParam Long userId2)
+    public void createChat(@RequestParam Long userId1, @RequestParam Long userId2)
     {
-        // TODO Use Service and do not create a new chat every time
-        Optional<UserAccount> user1 = userAccount.findById(userId1);
-        Optional<UserAccount> user2 = userAccount.findById(userId2);
-        if(user1.isPresent() && user2.isPresent())
-        {
-            UserAccount u1 = user1.get();
-            UserAccount u2 = user2.get();
-            Chat chat = new Chat();
-            chat.getUsers().add(u1);
-            chat.getUsers().add(u2);
-
-            return chatRepository.save(chat);
-        }
-        return null;
+        chatService.createChat(userId1,userId2);
     }
 
     @MessageMapping("/sendMessage")
-    public ChatMessage sendMessage(@Payload ChatMessage chatMessage)
+    public void sendMessage(@Payload ChatMessage chatMessage)
     {
-        chatMessageRepository.save(chatMessage);
-        chatMessageRepository.save(chatMessage);
-        System.out.println("--------------------------------------");
-        messagingTemplate.convertAndSend("/topic", chatMessage);
-        return chatMessage;
+        chatService.sendMessage(chatMessage);
     }
 
 }
