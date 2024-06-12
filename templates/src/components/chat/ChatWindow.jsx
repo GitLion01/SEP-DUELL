94397d4a6c43767b2f112c7e1f444fae75b90a07
Saman Schero <saman.schero@uni-due.de>
2024-06-13 00:08:42 +0200

    Chat funktioniert ansatzweise

--- templates/src/components/chat/ChatWindow.jsx
+++ templates/src/components/chat/ChatWindow.jsx
@@ -1,117 +1,107 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { WebSocketContext} from "../../WebSocketProvider";
+import { WebSocketContext } from "../../WebSocketProvider"; // Passe den Pfad entsprechend an
 import './ChatWindow.css';
 import './ChatBubble.css';
 
-const generateChatId = (userId, targetId) => {
-  return [userId, targetId].sort().join('-');
-};
-
 function ChatWindow({ chatTarget, type }) {
-  const [messages, setMessages] = useState({});
+  const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState('');
-  const [editingMessage, setEditingMessage] = useState(null);
-  const userId = localStorage.getItem('id');
-  const chatId = generateChatId(userId, chatTarget.id);
+  const [editingMessageId, setEditingMessageId] = useState(null);
+  const userId = parseInt(localStorage.getItem('id'), 10);
+  const chatId = 1;
   const { chatClient } = useContext(WebSocketContext);
 
-  // Nachrichten vom Server laden
-  useEffect(() => {
-    if (chatId) {
-      const fetchMessages = async () => {
-        try {
-          const response = await fetch(`http://localhost:8080/messages/${chatId}`);
-          const data = await response.json();
-          setMessages((prevMessages) => ({
-            ...prevMessages,
-            [chatId]: Array.isArray(data) ? data : [], // Ensure data is an array
-          }));
-        } catch (error) {
-          console.error('Fehler beim Laden der Nachrichten:', error);
-        }
-      };
+  const normalizeMessage = (message) => {
+    // Passe die Struktur der empfangenen Nachricht an das erwartete Format an
+    return {
+      id: message.id,
+      message: message.message,
+      chat: { id: message.chatId },
+      sender: { id: message.senderId }
+    };
+  };
 
-      fetchMessages();
-    }
-  }, [chatId]);
-
-  // WebSocket-Verbindung herstellen und Nachrichten empfangen
   useEffect(() => {
     if (chatClient && chatId) {
-      chatClient.subscribe(`/topic/${chatId}`, (message) => {
+      chatClient.subscribe(`/user/${userId}/queue/messages`, (message) => {
         const receivedMessage = JSON.parse(message.body);
-        setMessages((prevMessages) => {
-          const updatedMessages = { ...prevMessages };
-          if (!Array.isArray(updatedMessages[receivedMessage.chat.id])) {
-            updatedMessages[receivedMessage.chat.id] = [];
-          }
-          updatedMessages[receivedMessage.chat.id].push(receivedMessage);
-          return updatedMessages;
-        });
+        if (receivedMessage && receivedMessage.id && receivedMessage.senderId && receivedMessage.chatId) {
+          const normalizedMessage = normalizeMessage(receivedMessage);
+          setMessages((prevMessages) => [...prevMessages, normalizedMessage]);
+        } else {
+          console.error("Received message is missing required properties:", receivedMessage);
+        }
       });
     }
-  }, [chatClient, chatId]);
+  }, [chatClient, chatId, userId]);
 
-  // Nachricht senden und speichern
   const handleSendMessage = () => {
     if (newMessage.trim() !== '') {
       const message = {
-        id: null, // ID wird vom Backend gesetzt
+        id: Date.now(), // Temporäre ID für die lokale Anzeige
         message: newMessage,
         chat: { id: chatId },
         sender: { id: userId }
       };
+
+      // Nachricht lokal hinzufügen, um sofortige Anzeige zu ermöglichen
+      setMessages((prevMessages) => [...prevMessages, message]);
+
+      // Nachricht an den Server senden
       chatClient.publish({ destination: '/chat/sendMessage', body: JSON.stringify(message) });
 
-      setMessages(prev => {
-        const updatedMessages = { ...prev };
-        if (!Array.isArray(updatedMessages[chatId])) {
-          updatedMessages[chatId] = [];
-        }
-        updatedMessages[chatId].push(message);
-        return updatedMessages;
-      });
       setNewMessage('');
     }
   };
 
-  // Nachricht bearbeiten
-  const handleEditMessage = (message) => {
-    setEditingMessage(message);
-    setNewMessage(message.message);
+  const handleEditMessage = (messageId, currentMessage) => {
+    setEditingMessageId(messageId);
+    setNewMessage(currentMessage);
   };
 
   const handleUpdateMessage = () => {
-    if (editingMessage && newMessage.trim() !== '') {
-      const updatedMessage = { ...editingMessage, message: newMessage };
-      chatClient.publish({ destination: '/chat/editMessage', body: JSON.stringify(updatedMessage) });
+    if (editingMessageId && newMessage.trim() !== '') {
+      const message = {
+        id: editingMessageId,
+        message: newMessage,
+        chat: { id: chatId },
+        sender: { id: userId }
+      };
 
-      setMessages((prevMessages) => {
-        const updatedMessages = { ...prevMessages };
-        updatedMessages[chatId] = updatedMessages[chatId].map((msg) =>
-          msg.id === editingMessage.id ? updatedMessage : msg
-        );
-        return updatedMessages;
-      });
-      setEditingMessage(null);
+      // Nachricht lokal aktualisieren
+      setMessages((prevMessages) =>
+        prevMessages.map((msg) =>
+          msg.id === editingMessageId ? message : msg
+        )
+      );
+
+      // Nachricht an den Server senden
+      chatClient.publish({ destination: '/chat/editMessage', body: JSON.stringify(message) });
+
       setNewMessage('');
+      setEditingMessageId(null);
     }
   };
 
-  // Nachricht löschen
-  const handleDeleteMessage = (message) => {
-    chatClient.publish({ destination: '/chat/deleteMessage', body: JSON.stringify(message) });
+  const handleDeleteMessage = (messageId) => {
+    const message = {
+      id: messageId,
+      chat: { id: chatId },
+      sender: { id: userId }
+    };
 
-    setMessages((prevMessages) => {
-      const updatedMessages = { ...prevMessages };
-      updatedMessages[chatId] = updatedMessages[chatId].filter((msg) => msg.id !== message.id);
-      return updatedMessages;
-    });
+    // Nachricht lokal löschen
+    setMessages((prevMessages) =>
+      prevMessages.filter((msg) => msg.id !== messageId)
+    );
+
+    // Nachricht an den Server senden
+    chatClient.publish({ destination: '/chat/deleteMessage', body: JSON.stringify(message) });
   };
 
   const handleKeyPress = (event) => {
     if (event.key === 'Enter') {
-      if (editingMessage) {
+      if (editingMessageId) {
         handleUpdateMessage();
       } else {
         handleSendMessage();
@@ -119,23 +109,18 @@ function ChatWindow({ chatTarget, type }) {
     }
   };
 
-  // Funktion zum Setzen des Lesestatus
   const handleReadMessage = (message) => {
     if (message.sender.id !== userId && !message.read) {
       const updatedMessage = { ...message, read: true };
       chatClient.publish({ destination: '/chat/readMessage', body: JSON.stringify(updatedMessage) });
 
-      setMessages((prevMessages) => {
-        const updatedMessages = { ...prevMessages };
-        updatedMessages[chatId] = updatedMessages[chatId].map((msg) =>
+      setMessages((prevMessages) =>
+        prevMessages.map((msg) =>
           msg.id === message.id ? { ...msg, read: true } : msg
-        );
-        return updatedMessages;
-      });
+        )
+      );
     }
   };
-
-  const chatMessages = Array.isArray(messages[chatId]) ? messages[chatId] : [];
 
   if (!chatTarget || !chatTarget.id) {
     return <div>Wählen Sie einen Freund oder eine Gruppe aus, um den Chat zu starten</div>;
@@ -145,18 +130,18 @@ function ChatWindow({ chatTarget, type }) {
     <div className="chat-window">
       <h2>Chat mit {type === 'friend' ? chatTarget.username : chatTarget.name}</h2>
       <div className="chat-messages imessage">
-        {chatMessages.map((message, index) => (
+        {messages.map((message, index) => (
           <p
             key={index}
-            className={message.sender.id === userId ? 'from-me' : 'from-them'}
+            className={message.sender && message.sender.id === userId ? 'from-me' : 'from-them'}
             onClick={() => handleReadMessage(message)}
           >
-            {type === 'group' && message.sender.id !== userId && (
+            {type === 'group' && message.sender && message.sender.id !== userId && (
               <span className="message-sender">{message.sender.username}</span>
             )}
             <span className="message-text">{message.message}</span>
             <div className="message-actions">
-              <button onClick={() => handleEditMessage(message)}>
+              <button onClick={() => handleEditMessage(message.id, message.message)}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
@@ -168,7 +153,7 @@ function ChatWindow({ chatTarget, type }) {
                   <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z" />
                 </svg>
               </button>
-              <button onClick={() => handleDeleteMessage(message)}>
+              <button onClick={() => handleDeleteMessage(message.id)}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
@@ -194,8 +179,8 @@ function ChatWindow({ chatTarget, type }) {
           onKeyPress={handleKeyPress}
           placeholder="Nachricht..."
         />
-        <button onClick={editingMessage ? handleUpdateMessage : handleSendMessage}>
-          {editingMessage ? 'Update' : 'Send'}
+        <button onClick={editingMessageId ? handleUpdateMessage : handleSendMessage}>
+          {editingMessageId ? 'Update' : 'Send'}
         </button>
       </div>
     </div>
