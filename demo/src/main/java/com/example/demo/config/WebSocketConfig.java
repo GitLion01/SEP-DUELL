66a7e28a393479214ea5387d01650ca2507b4aa0
Saman Schero <saman.schero@uni-due.de>
2024-06-13 14:37:18 +0200

    .

--- demo/src/main/java/com/example/demo/config/WebSocketConfig.java
+++ demo/src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -2,6 +2,8 @@ package com.example.demo.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.WebSocketSession;
+
 
 
 @Configuration
