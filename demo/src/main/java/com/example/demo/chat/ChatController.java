72933d89fbc451f2789337ca929f2b531ba22685
Danny Nasra <danny.nasra@stud.uni-due.de>
2024-06-13 12:57:07 +0200

    send groupp message function not needed every thing is working implemente DONE

--- demo/src/main/java/com/example/demo/chat/ChatController.java
+++ demo/src/main/java/com/example/demo/chat/ChatController.java
@@ -1,5 +1,6 @@
 package com.example.demo.chat;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.web.bind.annotation.*;
@@ -15,15 +16,28 @@ public class ChatController {
     private final ChatService chatService;
 
     @PostMapping("/create.chat")
-    public void createChat(@RequestParam Long userId1, @RequestParam Long userId2)
+    public ResponseEntity<Long> createChat(@RequestParam Long userId1, @RequestParam Long userId2)
     {
-        chatService.createChat(userId1,userId2);
+        return chatService.createChat(userId1,userId2);
     }
 
     @PostMapping("/create.group")
-    public void createGroup(@RequestBody List<Long> userIds, @RequestParam String groupName){
-        chatService.createGroup(userIds,groupName);
+    public ResponseEntity<Long> createGroup(@RequestBody List<Long> userIds, @RequestParam String groupName){
+        return chatService.createGroup(userIds,groupName);
     }
+
+    @GetMapping("/get.messages")
+    public ResponseEntity<List<ChatMessageDTO>> getChat(@RequestParam Long chatId)
+    {
+        return chatService.getMessages(chatId);
+    }
+
+    @GetMapping("/get.groups")
+    public ResponseEntity<List<GroupDTO>> getGroups(@RequestParam Long userId)
+    {
+        return chatService.getGroups(userId);
+    }
+
 
     @MessageMapping("/sendMessage")
     public void sendMessage(@Payload ChatMessage chatMessage)
@@ -31,11 +45,11 @@ public class ChatController {
         chatService.sendMessage(chatMessage);
     }
 
-    @MessageMapping("/send-group-message")
+    /*@MessageMapping("/send-group-message")
     public void sendGroupMessage(@Payload ChatMessage chatMessage)
     {
         chatService.sendGroupMessage(chatMessage);
-    }
+    }*/
 
     @MessageMapping("/editMessage")
     public void editMessage(@Payload ChatMessage chatMessage)
