907c07fdbc0b694813d96b951e4944ad21eb0019
Özgürcan Cevlani <oezguercan.cevlani@stud.uni-due.de>
2024-06-14 13:10:57 +0200

    - ProfileController + UserAccountResponse + UserProfileResponse muss der Besitzer seinen Namen eintragen
    todo: UserAccountService enterDuel und exitDuel und duel status
    
    todo: Suche für Leaderboard nach ID
    
    todo: Merge mit Beyza

--- demo/src/main/java/com/example/demo/leaderboard/LeaderboardService.java
+++ demo/src/main/java/com/example/demo/leaderboard/LeaderboardService.java
@@ -1,6 +1,6 @@
 // src/main/java/com/example/demo/leaderboard/LeaderboardService.java
 package com.example.demo.leaderboard;
-
+import com.example.demo.game.GameRepository;            //für duell Status
 import com.example.demo.user.UserAccount;
 import com.example.demo.user.UserAccountRepository;
 import lombok.AllArgsConstructor;
@@ -15,6 +15,7 @@ import java.util.stream.Collectors;
 public class LeaderboardService {
     private final UserAccountRepository userAccountRepository;
     private final SimpMessagingTemplate messagingTemplate;
+    private final GameRepository gameRepository;                    //für duell Status
 
     public List<UserAccount> getLeaderboard() {
         return userAccountRepository.findAll().stream()
@@ -26,9 +27,14 @@ public class LeaderboardService {
         UserAccount user = userAccountRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("User not found"));
 
+        // Check if user is in a game
+        // für duell Status von
+        if (gameRepository.existsByUsersContaining(user)) {
+            status = "im Duell";
+        }                                  //für duell Status bis
+
         user.setStatus(status);
         userAccountRepository.save(user);
         messagingTemplate.convertAndSend("/topic/leaderboard", user);   ///topic/leaderboard   //user-Objekt = alle user nicht eine einzelne userid
     }
 }
-//test test
