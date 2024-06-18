a4025d7d0eefc5df26ee49bb23e01b9d7f6df3e6
Danny Nasra <danny.nasra@stud.uni-due.de>
2024-06-18 12:53:10 +0200

    g

--- demo/src/main/java/com/example/demo/game/GameRepository.java
+++ demo/src/main/java/com/example/demo/game/GameRepository.java
@@ -2,16 +2,25 @@ package com.example.demo.game;
 
 import com.example.demo.user.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
 
-    Optional<Game> findByidAndId(Long Id, Long userID);
+    Optional<Game> findByIdAndId(Long Id, Long userID);
 
     boolean existsByUsersContaining(UserAccount user);
 
+    @Transactional
+    @Query(value = "SELECT game_id FROM game_users where user_id in (:userID)", nativeQuery = true)
+    boolean findByUserId(Long userID);
+
+    @Transactional
+    @Query(value = "SELECT game_id FROM game_users where user_id in (:userID)", nativeQuery = true)
+    Optional<Game> findByUserIdOptional(Long userID);
 
 }
