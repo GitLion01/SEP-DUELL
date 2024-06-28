1702102d5fb0553fa7b78a8d49ff92bbed186726
Enes Coskun <enes.coskun@stud.uni-due.de>
2024-06-28 13:37:01 +0200

    Dateien für Livestream + Button auf Startseite erstellt

--- templates/src/components/Duell/DuelField.jsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-import Card from '../card';
-import './DuelField.css';
---
+++ templates/src/components/Livestream/LiveTabelle.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import BackButton from '../BackButton';
+import "./LiveTabelle.css";
+import { WebSocketContext } from "../../WebSocketProvider";
+
+const LiveTabelle = () => {
+
+}
