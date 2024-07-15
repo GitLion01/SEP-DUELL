962e15ce22a9d334b08a37b45e0784b21f7a4306
Soufian Kkhennousse <soufian.khennousse@stud.uni-due.de>
2024-07-15 13:12:46 +0200

    fetchType Lazy entfernt aus game

--- demo/src/main/java/com/example/demo/game/Game.java
+++ demo/src/main/java/com/example/demo/game/Game.java
@@ -24,7 +24,7 @@ public class Game {
     private Boolean streamed = false;
     private Long botDeckId;
 
-    @OneToMany(fetch = FetchType.LAZY)
+    @OneToMany
     @JoinTable(
             name = "game_users",
             joinColumns = @JoinColumn(name = "game_id"),
@@ -33,11 +33,11 @@ public class Game {
     @JsonIgnore
     private List<UserAccount> users = new ArrayList<>();
 
-    @OneToMany(fetch = FetchType.LAZY)
+    @OneToMany
     @JsonIgnore
     private List<UserAccount> viewers = new ArrayList<>();
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne
     private PlayerState playerStateBot;
 
     @OneToOne(fetch = FetchType.EAGER)
